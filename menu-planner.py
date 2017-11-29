082fc464f58130a08bb392e8b481fb49325c3a3f
Laura Ekstrand <laura@jlekstrand.net>
2017-11-28 21:00:32 -0800

    Has cute styling now.

--- menu-planner.py
+++ menu-planner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.5
 from datetime import date
 from recipes import Recipes
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 @app.route("/")
